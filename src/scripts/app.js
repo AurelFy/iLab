146a9a14afd827d7e005de4f52e096a3fc0e4731
Aurélien Feye <aurelien.feye@student.heaj.be>
2025-10-10 17:00:50 +0200

    0.3.1-website

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,39 @@
-console.info('Hello world');
+"use strict";
+
+/////////// IMPORT LIBRAIRIE
+
+import { gsap } from "gsap";
+
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { SplitText } from "gsap/SplitText"; 
+
+gsap.registerPlugin(SplitText, ScrollTrigger); 
+
+
+//////////// ANIMATIONS
+
+// window.addEventListener("DOMContentLoaded", () => {
+
+//     document.fonts.ready.then(() => {
+//         const split = new SplitText(".head__title--big", { type: "chars" });
+//         const splitChars = split.chars; 
+    
+//         gsap.from(splitChars, {
+//             x: -50, 
+//             opacity: 0, 
+//             duration: 1.3, 
+//             stagger: 0.3
+//         }); 
+//     });
+// }); 
+
+
+// const letterTitle = document.querySelectorAll('.head__title--span'); 
+
+// gsap.from(letterTitle, {
+//     x: -50, 
+//     opacity: 0, 
+//     duration: 1, 
+//     stagger: 0.3, 
+//     ease: "power2.inOut"
+// }); 
