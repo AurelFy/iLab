c0d61dc84211f152c9de7fb935221810026899c0
Aurélien Feye <aurelien.feye@student.heaj.be>
2025-10-14 16:47:15 +0200

    0.4.1-website(animations)

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -9,31 +9,92 @@ import { SplitText } from "gsap/SplitText";
 
 gsap.registerPlugin(SplitText, ScrollTrigger); 
 
+//console.log(SplitText);
+
 
 //////////// ANIMATIONS
 
-// window.addEventListener("DOMContentLoaded", () => {
 
-//     document.fonts.ready.then(() => {
-//         const split = new SplitText(".head__title--big", { type: "chars" });
-//         const splitChars = split.chars; 
+if(document.fonts){
+
+    const split = new SplitText('.head__title--big', { type: 'chars' }); 
+
+    gsap.from(split.chars, {
+        x: -50, 
+        opacity: 0, 
+        duration: 0.6, 
+        stagger: 0.2
+    }); 
+
+    gsap.from('.head__title--small', {
+        scale: 0.5, 
+        opacity: 0, 
+        delay: 1.5, 
+        duration: 0.8, 
+        ease: 'power2.out'
+    });
+}
+
+const introText = document.querySelector('.intro__text'); 
+    const splitIntro = new SplitText('.intro__text', { type: 'words' }); 
+    const splitIntroDesk = new SplitText('.intro__text', { type: 'lines' }); 
+
+if (window.innerWidth < 768) {
+
     
-//         gsap.from(splitChars, {
-//             x: -50, 
-//             opacity: 0, 
-//             duration: 1.3, 
-//             stagger: 0.3
-//         }); 
-//     });
-// }); 
 
+    if(introText) {
 
-// const letterTitle = document.querySelectorAll('.head__title--span'); 
+    gsap.from(splitIntro.words, {
+        scrollTrigger: {
+            trigger: '.intro', 
+            start: 'top 80%', 
+            toggleActions: 'restart reset restart reset', 
+        }, 
+        y: 100, 
+        opacity: 0, 
+        duration: 1, 
+        stagger: 0.2,
+        ease: 'power2.out'
+    }); 
+    } 
 
-// gsap.from(letterTitle, {
-//     x: -50, 
-//     opacity: 0, 
-//     duration: 1, 
-//     stagger: 0.3, 
-//     ease: "power2.inOut"
-// }); 
+    gsap.from('.intro__text--mark', {
+        scrollTrigger: {
+            trigger: 'intro', 
+            start: 'top 50%', 
+            toggleActions: 'restart reset restart reset'
+        }, 
+        delay: 0.8,  
+        stagger: 0.5,
+        backgroundImage: 'linear-gradient(to left, rgba(235, 242, 240, 0), rgba(235, 242, 240, 0), rgba(235, 242, 240, 0), rgba(235, 242, 240, 0), rgba(235, 242, 240, 0))' , 
+        ease: 'power2.out'
+    }); 
+
+} else {
+    gsap.from(splitIntroDesk.lines, {
+        scrollTrigger: {
+            trigger: '.intro', 
+            start: 'top 60%', 
+            toggleActions: 'restart reset restart reset', 
+        }, 
+        y: 100, 
+        opacity: 0, 
+        duration: 1, 
+        stagger: 0.2,
+        ease: 'power2.out'
+    }); 
+
+    gsap.from('.intro__text--mark', {
+        scrollTrigger: {
+            trigger: 'intro', 
+            start: 'top 50%', 
+            toggleActions: 'restart reset restart reset'
+        }, 
+        delay: 0.6,  
+        stagger: 0.5,
+        backgroundImage: 'linear-gradient(to left, rgba(235, 242, 240, 0), rgba(235, 242, 240, 0), rgba(235, 242, 240, 0), rgba(235, 242, 240, 0), rgba(235, 242, 240, 0))' , 
+        ease: 'power2.out'
+    }); 
+}
+
